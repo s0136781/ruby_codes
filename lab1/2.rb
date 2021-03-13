c0f207b9e1fc5336a40154df82c2fd2ef7581bbf
Ksardas <vlad504662@gmail.com>
2021-03-13 13:42:53 +0300

    2 Задание
    1 лаба

---
+++ lab1/2.rb
@@ -0,0 +1,14 @@
+puts "Hello, #{ARGV[0]}"
+
+puts "Your favorite language, #{ARGV[0]}?"
+language = STDIN.gets.chomp #считывание входных данных
+if language == "ruby"
+    puts "you podliza"
+else
+    puts "Skoro budet ruby"
+end
+t = STDIN.gets
+<<<<<<< HEAD
+
+=======
+>>>>>>> master
