ee0b4d8ff873375b90217b63984b533028439a75
Ksardas <vlad504662@gmail.com>
2021-03-13 15:42:04 +0300

    3.1  дополнение
    1 лаба

--- lab1/3.rb
+++ lab1/3.rb
@@ -2,3 +2,8 @@ puts "#{ARGV[0]}, input command of OS?"
 oscommand = STDIN.gets.chomp
 system(oscommand)
 t = STDIN.gets
+
+puts "#{ARGV[0]}, input command of Ruby?"
+rubycommand = STDIN.gets.chomp
+system("ruby -e \"#{rubycommand}\"")
+t = STDIN.gets
